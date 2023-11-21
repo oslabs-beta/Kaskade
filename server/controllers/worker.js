c52e2d7c7e7327e45946d21078dfab578584a628
Wenzhen Gong <kong20100902@hotmail.com>
2023-11-20 19:55:51 -0800

    deleted filed no longer needed

--- server/controllers/worker.js
+++
@@ -1,7 +0,0 @@
-const { parentPort } = require("worker_threads");
-const db = require("../models/soloProjectModels");
-
-const findAllUsers = `SELECT * FROM users`;
-db.query(findAllUsers).then((data) => {
-  parentPort.postMessage(data.rows);
-});
