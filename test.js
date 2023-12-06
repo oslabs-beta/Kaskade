686ea263444971fca164f85cc4807e5c83f6a035
Michael Mann <michaelmannw@gmail.com>
2023-12-05 22:36:27 -0500

    (test)test.js: just a test file for our initial test
    - The first inputted request can be our initial test, I just added a few other requests for later testing

--- test.js
+++ test.js
@@ -1,3 +1,61 @@
-const opts = {'a': 1, 'b': 2}
+const testConfig = {
+    servers: [
+      "localhost:3000"
+    ],
+    testDuration: 600,
+    concurrentUsers: 100,
+    targetThroughput: 1000,
+    numOfWorkers: 1,
+    sessions: [
+      {
+        sessionName: "test session",
+        requests: [
+          {
+            requestName: "get all users",
+            url: "/api/getallusers",
+            method: "GET",
+            headers: {
+              "Content-type": "application/json"
+            }
+          },
+          {
+            requestName: "create user",
+            url: "/api/createuser",
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: {
+              fn: "Kaskade",
+              ln: "Test",
+              email: "kaskade@gmail.com",
+              pw: "1234"
+            }
+          },
+          {
+            requestName: "delete order",
+            url: "/api/deleteorder?orderid=db289395-fe68-47af-aadc-f659746d2559",
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json"
+            },
+          },
+          {
+            requestName: "create order",
+            url: "localhost:3000/api/createorder",
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: {
+              link: "Kaskade",
+              note: "test"
+            }
+  
+          }
+        ]
+      }
+    ]
+  };
 
-console.log(!'a' in opts)
+// console.log("Stringified object if needed: ", JSON.stringify(testConfig))
