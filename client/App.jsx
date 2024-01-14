40a02cf834cdba5bfa07df8f54c5d08808e64952
jingjing <jingjingwangacc@gmail.com>
2024-01-14 14:50:00 -0800

    frontend boilerplate

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Pages /> */}
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
