9aff6204be38551730a6d09344cf695766b44d34
Wenzhen Gong <kong20100902@hotmail.com>
2024-01-19 22:06:30 -0800

    Seems to get vite work with electron. HMR tested. When started, needs to load. Weird

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
+import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: './client/'
+  plugins: [
+    react(),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`.
+        entry: 'main.js',
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`.
+        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+        input: path.join(__dirname, 'preload.js'),
+      },
+      // Ployfill the Electron and Node.js built-in modules for Renderer process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      renderer: {},
+    }),
+  ],
+  root: 'client/'
+
 })
