69f337f3ad39344572e541947b215dd3d202e22b
jingjing <jingjingwangacc@gmail.com>
2024-02-08 17:11:35 +0800

    headbar

--- client/common/HeadBar.jsx
+++ client/common/HeadBar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import TextField from '@mui/material/TextField';
+import { useSelector } from 'react-redux';
+import Autocomplete from '@mui/material/Autocomplete';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const HeadBar = () => {
     const HeadBarContainer = styled.div`
@@ -38,16 +42,51 @@ const HeadBar = () => {
     height:40px;
     `
 
-    const handleSearch = (e) => {
-        console.log(e.target.value);
+    const navigate = useNavigate();
+
+    // Get all sessions from data file.
+    const allSessions = useSelector(state => state.data.datafile);
+
+    // Prepare all search values for the autocomplete search bar.
+    const allValues = [];
+    // For each value in "allValues", generate its corresponding redirect URL.
+    const allURLs = [];
+    for (let i = 0; i < allSessions.length; i++) {
+        // The session itself.
+        allValues.push(allSessions[i].sessionName);
+        allURLs.push("/sessions/" + allSessions[i].sessionId);
+
+        // All requests in this session.
+        for (let j = 0; j < allSessions[i].requests.length; j++) {
+            allValues.push(allSessions[i].sessionName + "  >>  " + allSessions[i].requests[j].requestName);
+            allURLs.push("/sessions/" + allSessions[i].sessionId + "/" + allSessions[i].requests[j].requestId);
+        }
     }
     
+    const handleSelect = (e, newValue) => {
+        console.log("Selection is: ", newValue);
+        for (let i = 0; i < allValues.length; ++i) {
+            if (allValues[i] === newValue) {
+                console.log("Selection's Id is: ", i);
+                console.log("Selection's URL is: ", allURLs[i]);
+                navigate(allURLs[i]);
+            }
+        }
+    }
 
     return (
         <HeadBarContainer>
             <LogoDiv>KASKADE</LogoDiv>
             <SearchDiv>
-                <div><TextField id="outlined-basic" label="Search" variant="outlined" onChange={handleSearch}/></div>
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={allValues}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Movie" />}
+                    onChange={handleSelect}
+                />
+                {/* <div><TextField id="outlined-basic" label="Search" variant="outlined" onChange={handleSearch}/></div> */}
             </SearchDiv>
         </HeadBarContainer>
     )
