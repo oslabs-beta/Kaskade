6ff7252df416ff916b1bfbb67dfad9e2a7a8ef19
Wenzhen Gong <kong20100902@hotmail.com>
2024-02-14 17:26:27 -0800

    removed worker console.log

--- core/worker.js
+++ core/worker.js
@@ -4,7 +4,7 @@ const runner =require('./runner')
 
 const { Worker, threadId, isMainThread, parentPort, workerData } = require('node:worker_threads');
 const opts = workerData;
-console.log('Worker', threadId, 'is processing', opts.concurrentUsersforworker, 'users\' requests');
+// console.log('Worker', threadId, 'is processing', opts.concurrentUsersforworker, 'users\' requests');
 if (!isMainThread) {  
   const metricsArr = []
   for(let i = 0; i< opts.concurrentUsersforworker; i++){
